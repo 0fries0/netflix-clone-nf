4b6609b1c4c5e48f960b8b4dcd2e97b02fe27e33
Ritika Tripathi <71340872+0fries0@users.noreply.github.com>
2022-02-02 01:40:08 +0530

    Add files via upload

---
+++ browse.js
@@ -0,0 +1,14 @@
+// const playBtn = document.querySelector(".play-button");
+// const vid = document.querySelector(".video-player");
+// const container = document.querySelector(".entire-screen");
+
+
+// playBtn.addEventListener("click", ()=>{
+//     const video = document.createElement("video");
+//     video.setAttribute("src", "/Images/films/drama/video.mp4");
+//     video.setAttribute("controls", "");
+//     video.setAttribute("width", "550px");
+//     vid.appendChild(video);
+// })
+
+
